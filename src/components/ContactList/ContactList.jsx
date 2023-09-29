4dbd45023d8be06517b846087e07b4e600bebe10
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-09-30 01:55:44 +0200

    usuwanie kontaktów

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './ContactList.module.css';
 import PropTypes from 'prop-types';
+import { deleteContact } from '../../Redux/ContactsReducer'; // Zaimportuj akcję usuwania
 
-const Contact = ({ id, name, number }) => {
+const Contact = ({ id, name, number, onDeleteClick }) => {
   return (
     <li className={styles.listItem}>
       {name}: {number}
+      <button onClick={() => onDeleteClick(id)}>Delete</button>
     </li>
   );
 };
@@ -15,23 +17,35 @@ Contact.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
 };
 
 const ContactList = () => {
   const contacts = useSelector((state) => state.contacts.contacts);
   const filter = useSelector((state) => state.contacts.filter);
+  const dispatch = useDispatch(); 
 
-  
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter)
   );
+
+  const handleDeleteContact = (contactId) => {
+   
+    dispatch(deleteContact(contactId));
+  };
 
   return (
     <div>
       {filteredContacts.length > 0 ? (
         <ul className={styles.contactList}>
           {filteredContacts.map((contact) => (
-            <Contact key={contact.id} id={contact.id} {...contact} />
+            <Contact
+              key={contact.id}
+              id={contact.id}
+              name={contact.name}
+              number={contact.number}
+              onDeleteClick={handleDeleteContact} 
+            />
           ))}
         </ul>
       ) : (
